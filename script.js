c7acae103848d54fd208c67105294324674ec7a8
M A Razvi <abuzarflw@gmail.com>
2022-04-07 23:07:28 -0700

    Change colors for past and present timeboxes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ dateDisplay.textContent = date;
 var currentHour = moment().hour();
 console.log(currentHour);
 var workingHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-// var workingHours = [16,17,18,19,20,21,22,23,24]
+// var workingHours = [16,17,18,19,20,21,22,23,24];
 var containerbox = document.querySelector(".container");
 //Function to style timeboxes according to current time
 //Function is called every second and it updates the stling accordingly
@@ -18,13 +18,13 @@ var keepChecking = setInterval(function() {
     {
         if(currentHour === workingHours[i]) {
             //Styling for present
-            containerbox.children[i].style.backgroundColor = "rgb(218, 216, 216)"; 
+            containerbox.children[i].style.backgroundColor = "rgb(236, 106, 128)"; 
             containerbox.children[i].classList.add("mb-3");
             containerbox.children[i].classList.add("mt-3");
         }
         else if(currentHour > workingHours[i]) {
             //Styling for past
-            containerbox.children[i].style.backgroundColor = "rgb(236, 106, 128)";
+            containerbox.children[i].style.backgroundColor = "rgb(218, 216, 216)";
         }
         else if(currentHour < workingHours[i]) {
             // Styling for future
