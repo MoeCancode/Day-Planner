1b4cb48b299ede6d6565e7cacbc3c2b84e31b3b7
M A Razvi <abuzarflw@gmail.com>
2022-04-07 19:35:03 -0700

    Making sure repo is up to date

---
+++ script.js
@@ -0,0 +1,6 @@
+var date = moment().format("dddd, MMMM Do YYYY");
+var dateDisplay = document.querySelector("#currentDay");
+
+dateDisplay.textContent = moment().format("dddd, MMMM Do YYYY");
+
+// dateDisplay.textContent = moment().format("dddd, MMMM Do YYYY");
