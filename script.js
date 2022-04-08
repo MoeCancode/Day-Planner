482489da5e8c84ec9818d44bddc4a98b58a32abc
M A Razvi <abuzarflw@gmail.com>
2022-04-07 22:59:30 -0700

    Make function that is called every second and it styles timeboxes according to past/present/future

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-var date = moment().format("dddd, MMMM Do YYYY");
 var dateDisplay = document.querySelector("#currentDay");
+var date = moment().format("dddd, MMMM Do");
 
-dateDisplay.textContent = moment().format("dddd, MMMM Do YYYY");
+//Display current date on webpage
+dateDisplay.textContent = date;
 
-console.log(moment());
+
+var currentHour = moment().hour();
+console.log(currentHour);
+var workingHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+// var workingHours = [16,17,18,19,20,21,22,23,24]
+var containerbox = document.querySelector(".container");
+//Function to style timeboxes according to current time
+//Function is called every second and it updates the stling accordingly
+
+var keepChecking = setInterval(function() {
+    for(var i = 0; i < workingHours.length; i++)
+    {
+        if(currentHour === workingHours[i]) {
+            //Styling for present
+            containerbox.children[i].style.backgroundColor = "rgb(218, 216, 216)"; 
+            containerbox.children[i].classList.add("mb-3");
+            containerbox.children[i].classList.add("mt-3");
+        }
+        else if(currentHour > workingHours[i]) {
+            //Styling for past
+            containerbox.children[i].style.backgroundColor = "rgb(236, 106, 128)";
+        }
+        else if(currentHour < workingHours[i]) {
+            // Styling for future
+            containerbox.children[i].style.backgroundColor = "#45cb45";
+        }
+    }
+
+
+}, 1000)
